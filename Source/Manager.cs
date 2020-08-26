c58a4050d7896e94fd5c362feb8094b15d36686d
Stevanus Ronald Riantono <StevanusRonald@gmail.com>
2020-08-26 18:05:20 -0500

    Implement Download Content from GitHub

---
+++ Source/Manager.cs
@@ -0,0 +1,168 @@
+﻿/*
+ * Copyright 2020 Stevanus Ronald Riantono
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace GitHubHelper.Libs {
+    public class Manager : IManager {
+        private readonly IWebRequestHelper _webRequestHelper;
+        private readonly string _tokenAuth;
+
+        public Manager(string tokenAuth)
+            : this(new WebRequestHelper(), tokenAuth) {
+        }
+
+        public Manager(IWebRequestHelper webRequestHelper, string tokenAuth) {
+            if (webRequestHelper == null) {
+                throw new ArgumentNullException("webRequestHelper");
+            }
+
+            if (String.IsNullOrEmpty(tokenAuth)) {
+                throw new ArgumentNullException("tokenAuth");
+            }
+            
+            _webRequestHelper = webRequestHelper;
+            _tokenAuth = tokenAuth;
+        }
+
+        private JToken GetRepoContent(string repoContentUrl) {
+            return _webRequestHelper.GetData<JToken>(repoContentUrl, out HttpStatusCode statusCode, null,
+                x => {
+                    x.Headers["Authorization"] = String.Format("Bearer {0}", _tokenAuth);
+                });
+        }
+
+        private bool ProcessFileRepoContent(JObject jFileRepoContent, string targetDirectory) {
+            var type = jFileRepoContent["type"].Value<string>();
+            if (String.IsNullOrEmpty(type) || !type.Equals("file", StringComparison.CurrentCultureIgnoreCase)) {
+                throw new ArgumentNullException("Invalid Type of Repository Content!");
+            }
+
+            if (!Directory.Exists(targetDirectory)) {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            var filename = jFileRepoContent["name"].Value<string>();
+            var targetFilename = Path.Combine(targetDirectory, filename);
+
+            var encoding = jFileRepoContent["encoding"].Value<string>();
+            switch (encoding) {
+                case "base64": {
+                    var encodedData = jFileRepoContent["content"].Value<string>();
+                    File.WriteAllBytes(targetFilename, Convert.FromBase64String(encodedData));
+                    return true;
+                }
+
+                default: {
+                    var downloadUrl = jFileRepoContent["download_url"].Value<string>();
+                    _webRequestHelper.DownloadFile(downloadUrl, targetFilename, out HttpStatusCode statusCode,
+                        x => {
+                            x.Headers["Authorization"] = String.Format("Bearer {0}", _tokenAuth);
+                        });
+                    return statusCode == HttpStatusCode.OK;
+                }
+            }
+        }
+
+        private int ProcessDirectoryRepoContent(JArray jDirectoryRepoContent, string targetDirectory) {
+            if (!Directory.Exists(targetDirectory)) {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            var totalProcessItem = 0;
+
+            foreach (var jItem in jDirectoryRepoContent) {
+                var name = jItem["name"].Value<string>();
+
+                var type = jItem["type"].Value<string>();
+                switch (type) {
+                    case "file": {
+                        var downloadUrl = jItem["download_url"].Value<string>();
+                        var targetFilename = Path.Combine(targetDirectory, name);
+                        _webRequestHelper.DownloadFile(downloadUrl, targetFilename, out HttpStatusCode statusCode,
+                            x => {
+                                x.Headers["Authorization"] = String.Format("Bearer {0}", _tokenAuth);
+                            });
+
+                        if (statusCode == HttpStatusCode.OK) {
+                            totalProcessItem++;
+                        }
+                        break;
+                    }
+
+                    case "dir": {
+                        var repoContentUrl = jItem["url"].Value<string>();
+                        var jRepoContent = GetRepoContent(repoContentUrl);
+                        var targetSubDirectory = Path.Combine(targetDirectory, name);
+                        totalProcessItem += ProcessRepoContent(jRepoContent, targetSubDirectory);
+                        break;
+                    }
+
+                    default:
+                        Debug.Fail("Type of Sub-Item from Directory Repo Content is not implemented!");
+                        break;
+                }
+            }
+
+            return totalProcessItem;
+        }
+
+        private int ProcessRepoContent(JToken jRepoContent, string targetDirectory) {
+            if (jRepoContent is JObject) {
+                if (ProcessFileRepoContent((JObject)jRepoContent, targetDirectory)) {
+                    return 1;
+                }
+            }
+            else if (jRepoContent is JArray) {
+                return ProcessDirectoryRepoContent((JArray)jRepoContent, targetDirectory);
+            }
+            else {
+                Debug.Fail("Json Type of Repo Content is not implemented!");
+            }
+
+            return 0;
+        }
+
+        public virtual string GenerateRepoContentUrl(string repoBlobOrTreeUrl) {
+            // Generate the URL? https://developer.github.com/v3/repos/contents/#get-repository-content
+            var uri = new Uri(repoBlobOrTreeUrl);
+
+            var urlType = uri.Segments[3];
+            if (String.IsNullOrEmpty(urlType) || urlType.Equals("blob") || urlType.Equals("tree")) {
+                throw new ArgumentException("Url is neither blob or tree.");
+            }
+
+            var owner = uri.Segments[1].Trim('\\', '/');
+            var repo = uri.Segments[2];
+            var branch = uri.Segments[4];
+            var relativePath = String.Join('/', uri.Segments.Skip(5).Select(x => x.Trim('\\', '/')));
+
+            return String.Format(@"{0}://{1}/api/v3/repos/{2}/{3}/contents/{4}?ref={5}",
+                uri.Scheme, uri.Host, owner, repo, relativePath, branch);
+        }
+
+        public int Download(string repoBlobOrTreeUrl, string targetDirectory) {
+            var repoContentUrl = GenerateRepoContentUrl(repoBlobOrTreeUrl);
+            var jRepoContent = GetRepoContent(repoContentUrl);
+            return ProcessRepoContent(jRepoContent, targetDirectory);
+        }
+    }
+}
